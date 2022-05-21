74bfb48bc3ee63c426d664b5b610488eaca3bef5
Daan van der Munnik <daan@munnik.net>
2022-05-21 13:36:50 +0200

    added database and docker stuff

---
+++ database/create-table.sql
@@ -0,0 +1,6 @@
+create table MyGuests (
+  id int not null auto_increment,
+  firstname varchar(255) not null,
+  lastname varchar(255) not null,
+  primary key (id)
+);
---
+++ database/insert-values.sql
@@ -0,0 +1,4 @@
+INSERT INTO MyGuests (firstname, lastname) VALUES ("Daan", "van der Munnik");
+INSERT INTO MyGuests (firstname, lastname) VALUES ("Jan", "Jansen");
+INSERT INTO MyGuests (firstname, lastname) VALUES ("Puk", "van de Petteflat");
+INSERT INTO MyGuests (firstname, lastname) VALUES ("Jan", "met de korte Achternaam");
